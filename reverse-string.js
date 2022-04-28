dbd815ce4166d47de448c008ffb5eae71f0c3339
Blair Bristow <blairhbristow@gmail.com>
2022-04-28 11:14:21 -0400

    Add solutions

--- reverse-string.js
+++ reverse-string.js
@@ -1,1 +1,16 @@
-export function reverse() {}
+export function reverse(string) {
+    let newString = ''
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        newString = newString + string[i]
+    }
+
+    return newString
+}
+
+export function reverseString(str) {
+    if (str === "")
+        return "";
+    else
+        return reverseString(str.substr(1)) + str.charAt(0);
+}
